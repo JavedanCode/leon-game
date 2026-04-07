0cb80116b147f19ecc4544d1408ef72ae8933ca6
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-04-07 20:55:45 +0300

    Change project name

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    publicPath: "/dinosaur-game/",
+    publicPath: "/leon-game/",
     clean: true,
   },
 });
