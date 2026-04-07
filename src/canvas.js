e85ece5e8579fb1fc4a56a6eb2d261e1cb1f83d4
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-04-07 22:28:13 +0300

    Add obstacle logic

--- src/canvas.js
+++ src/canvas.js
@@ -1,7 +1,7 @@
 export default function createCanvas() {
   const canvas = document.createElement("canvas");
   canvas.id = "game-canvas";
-  canvas.width = 1000;
+  canvas.width = 1600;
   canvas.height = 680;
 
   return canvas;
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const ctx = canvas.getContext("2d");
 
 const groundHeight = 50;
 
+const groundLevel = canvas.height - groundHeight;
+
 let ground1 = {
   x: 0,
   y: canvas.height - groundHeight,
@@ -23,10 +25,12 @@ let ground2 = {
   height: groundHeight,
 };
 
-function createObstacle() {
+let obstacles = [];
+
+function Obstacle() {
   return {
-    x: canvas.width, // start from right
-    y: canvas.height - groundHeight - 40, // sit on ground
+    x: canvas.width,
+    y: canvas.height - groundHeight - 40,
     width: 40,
     height: 40,
   };
@@ -34,23 +38,69 @@ function createObstacle() {
 
 let gameSpeed = 3;
 
-let spawnTimer = 0;
+const gapChances = [
+  { gap: 450, weight: 0.5 }, // 50%
+  { gap: 350, weight: 0.3 }, // 30%
+  { gap: 400, weight: 0.2 }, // 20%
+];
+
+function getRandomGap() {
+  let rand = Math.random();
+  let sum = 0;
+
+  for (let i = 0; i < gapChances.length; i++) {
+    sum += gapChances[i].weight;
+    if (rand < sum) {
+      return gapChances[i].gap;
+    }
+  }
+}
+
+let distanceSinceLastSpawn = 0;
+let nextSpawnDistance = getRandomGap();
 
 const player = {
-  x: 100,
+  x: 400,
   y: canvas.height - 2 * groundHeight,
   width: 50,
   height: 50,
+  jumpSpeed: -14,
+  gravity: -8,
+  verticalVelocity: 0,
 };
 function update() {
   ground1.x -= gameSpeed;
   ground2.x -= gameSpeed;
+
   if (ground1.x + ground1.width < 0) {
     ground1.x = ground2.x + ground2.width;
   }
 
   if (ground2.x + ground2.width < 0) {
     ground2.x = ground1.x + ground1.width;
+  }
+
+  distanceSinceLastSpawn += gameSpeed;
+
+  if (distanceSinceLastSpawn >= nextSpawnDistance) {
+    obstacles.push(Obstacle());
+
+    distanceSinceLastSpawn = 0;
+    nextSpawnDistance = getRandomGap();
+  }
+
+  for (let i = 0; i < obstacles.length; i++) {
+    obstacles[i].x -= gameSpeed;
+  }
+
+  obstacles = obstacles.filter((obs) => obs.x + obs.width > 0);
+
+  player.vy += player.gravity;
+  player.y += player.vy;
+
+  if (player.y + player.height > groundLevel) {
+    player.y = groundLevel - player.height;
+    player.vy = 0;
   }
 }
 function draw() {
@@ -70,6 +120,13 @@ function draw() {
   ctx.fillRect(ground2.x, ground2.y, ground2.width, ground2.height);
   ctx.fillStyle = "blue";
   ctx.fillRect(player.x, player.y, player.width, player.height);
+
+  ctx.fillStyle = "yellow";
+
+  for (let i = 0; i < obstacles.length; i++) {
+    let obs = obstacles[i];
+    ctx.fillRect(obs.x, obs.y, obs.width, obs.height);
+  }
 }
 function gameLoop() {
   update();
